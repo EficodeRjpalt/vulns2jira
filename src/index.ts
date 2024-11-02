d3030288426bf722c75c7571a887095938977573
Rasmus Paltschik <rjpalt@gmail.com>
2024-11-02 15:03:30 +0200

    Lint and update README.md

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,33 @@
-import * as core from '@actions/core';
-import * as github from '@actions/github';
-import { getDependabotAlerts } from './githubApi';
+import * as core from '@actions/core'
+// import * as github from '@actions/github'
+import { getDependabotAlerts } from './githubApi'
 
 // Define a type for the alert items
 type Alert = {
-  title: string;
-  recommendation: string;
-  createdAt: string;
-  cveLevel: string;
-};
+  title: string
+  recommendation: string
+  createdAt: string
+  cveLevel: string
+}
 
-async function run() {
+async function run () {
   try {
-    const token = core.getInput('github_token');
-    const repo = core.getInput('repo');  // Format: "owner/repo"
-    
+    const token = core.getInput('github_token')
+    const repo = core.getInput('repo')
+
     // Apply the Alert[] type to the alerts variable
-    const alerts: Alert[] = await getDependabotAlerts(token, repo);
+    const alerts: Alert[] = await getDependabotAlerts(token, repo)
 
     alerts.forEach((alert: Alert) => {
-      core.info(`Title: ${alert.title}`);
-      core.info(`Description: ${alert.recommendation}`);
-      core.info(`Created At: ${alert.createdAt}`);
-      core.info(`CVE Level: ${alert.cveLevel}`);
-    });
-
+      core.info(`Title: ${alert.title}`)
+      core.info(`Description: ${alert.recommendation}`)
+      core.info(`Created At: ${alert.createdAt}`)
+      core.info(`CVE Level: ${alert.cveLevel}`)
+    })
   } catch (error) {
     // Explicitly cast error to Error to access the message property
-    core.setFailed((error as Error).message);
+    core.setFailed((error as Error).message)
   }
 }
 
-run();
+run()
