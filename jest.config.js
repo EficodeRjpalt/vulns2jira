cbe1df17b49f82f8bbedc107edee178bc11c33de
EficodeRjpalt <rasmus.paltschik@eficode.com>
2024-11-10 09:20:27 +0200

    Update project

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/__tests__/**/*.test.ts'],
-  testPathIgnorePatterns: ['/node_modules/', '/dist/']
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  collectCoverage: true,
+  coverageReporters: ['text', 'cobertura']
 }
